744e8b22437b6ac29ea20c2fc820a3e7154716e1
EugenioUrbano <urbanoe348@gmail.com>
2025-02-24 07:38:02 -0300

    correcao pedidos econtext_processors

--- ifshop/context_processors.py
+++ ifshop/context_processors.py
@@ -1,19 +1,21 @@
 from .models import Pedido
 
 def pedidos_usuario(request):
-    if request.user.is_authenticated:
-        pedidos_feitos = Pedido.objects.filter(cliente=request.user)
+    pedidos_feitos = []
+    pedidos = []
     
-    else:
-        pedidos_feitos = []
-        for pedido in pedidos_feitos:
+    if request.user.is_authenticated:
+        pedidos = Pedido.objects.filter(cliente=request.user)
+    
+    for pedido in pedidos:
             imagem_principal = (
                 pedido.camiseta.imagens.filter(principal=True).first()
                 or pedido.camiseta.imagens.first()
             )
-        pedidos_feitos.append({
-            'pedido': pedido,
-            'imagem_principal': imagem_principal
-        })
+            pedidos_feitos.append({
+                'pedido': pedido,
+                'imagem_principal': imagem_principal
+            })
+
 
     return {"pedidos_feitos": pedidos_feitos}
