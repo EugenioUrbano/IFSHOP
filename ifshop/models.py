f8f430f22f9b9ae5cbde2ec99a01b6b0c16edbe6
EugenioUrbano <urbanoe348@gmail.com>
2025-08-29 11:02:41 -0300

    Models de produto e pedido de produto criado, admin supremo pode entra no gerenciamento de usuarios pela barra de usuario

--- ifshop/models.py
+++ ifshop/models.py
@@ -39,22 +39,7 @@ class UsuarioCustomizado(AbstractUser):
     
 ####################################################################################################
 
-class Camiseta(models.Model):
-    TAMANHOS_OPCOES = [
-        ('PP', 'PP'),
-        ('P', 'P'),
-        ('M', 'M'),
-        ('G', 'G'),
-        ('GG', 'GG'),
-	('XG', 'XG'),
-	('XGG', 'XGG')
-    ]
-    
-    ESTILOS_OPCOES = [
-        ('Babylook', 'BabyLook'),
-        ('Normal', 'Normal'),
-        ('Infantil', 'Infantil')
-    ]    
+class ProdutoBase(models.Model):
     TURNOS_OPCOES = [
         ('Matutino', 'Matutino'),
         ('Vespertino', 'Vespertino'),
@@ -67,39 +52,87 @@ class Camiseta(models.Model):
         ('Parcelado 2x Fisico', 'Parcelado 2x Fisica'),
         ('Negociar Pagamento', 'Negociar Pagamento'),
     ]
-
+    
     titulo = models.CharField(max_length=100)
+    
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     preco_parcela = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)
     forma_pag_op = models.CharField(max_length=200,null=True)
+    
     data_limite_pedidos = models.DateField()
     data_pag1 = models.DateField(help_text="Total ou primeira parcela")
     data_pag2 = models.DateField(null=True, blank=True,help_text="Não precisa colocar")
+    
     turnos = models.CharField(max_length=50)
-    cores = models.TextField( null=True, help_text="Digite as cores separadas por vírgula. Ex: azul, vermelho, verde")
     cursos = models.ManyToManyField('Curso')
+    
     imagem = models.ImageField(blank=True, null=True)
-    pix_qr_code_parcela = models.ImageField(upload_to='qrcode_parcela_camisetas/', null=False, default="")
-    pix_qr_code_total = models.ImageField(upload_to='qrcode_total_camisetas/', null=False, default="")
+    pix_qr_code_parcela = models.ImageField(upload_to='qrcode_parcela_produtos/', null=False, default="")
+    pix_qr_code_total = models.ImageField(upload_to='qrcode_total_produtos/', null=False, default="")
     pix_chave_parcela = models.TextField(max_length=300, null=False, default="")
     pix_chave_total = models.TextField(max_length=300, null=False, default="")
-    estilos = models.CharField(max_length=50, default="" )
-    tamanhos = JSONField(default=dict)
-    vendedor = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE, related_name="camisetas")
     
-    def lista_cores(self):
-        return [cor.strip() for cor in self.cores.split(",") if cor.strip()]
+    opcoes = models.TextField( null=True, help_text="Digite as opções separadas por vírgula. Ex: azul, vermelho, verde")
+    
+    vendedor = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE, related_name="produtos")
+    
+    def lista_opcoes(self):
+        return [opcao.strip() for opcao in self.opcoes.split(",") if opcao.strip()]
     
     def save(self, *args, **kwargs):
         if self.pk: 
-            pedidos = Pedido.objects.filter(camiseta=self)
+            pedidos = PedidoBase.objects.filter(camiseta=self)
             pedidos.update(revisado=False) 
             
-        self.cores = ", ".join(self.lista_cores())
+        self.opcoes = ", ".join(self.lista_opcoes())
         super().save(*args, **kwargs)
 
+class ImagemProdutoBase(models.Model):
+    produto = models.ForeignKey(ProdutoBase, related_name='imagens', on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='imagens_produtos/', null=True)
+    principal = models.BooleanField(default=False) 
+
+    def delete(self, *args, **kwargs):
+        if self.imagem and os.path.isfile(self.imagem.path):
+            os.remove(self.imagem.path)  
+        super().delete(*args, **kwargs)
+    
+    def save(self, *args, **kwargs):
+        if self.principal:
+            ImagemProdutoBase.objects.filter(produto=self.produto, principal=True).update(principal=False)
+        super().save(*args, **kwargs)
+
+class ProdutoDiverso(models.Model):
+    produto = models.ForeignKey(ProdutoBase, on_delete=models.CASCADE, related_name='diversos')
+    
     def __str__(self):
-        return self.titulo
+        return self.produto.titulo
+
+class Camiseta(models.Model):
+    TAMANHOS_OPCOES = [
+        ('PP', 'PP'),
+        ('P', 'P'),
+        ('M', 'M'),
+        ('G', 'G'),
+        ('GG', 'GG'),
+	    ('XG', 'XG'),
+	    ('XGG', 'XGG')
+    ]
+    
+    ESTILOS_OPCOES = [
+        ('Babylook', 'BabyLook'),
+        ('Normal', 'Normal'),
+        ('Infantil', 'Infantil')
+    ]    
+
+    produto = models.ForeignKey(ProdutoBase, on_delete=models.CASCADE, related_name='camisetas')
+
+    estilos = models.CharField(max_length=50, default="" )
+    tamanhos = JSONField(default=dict)
+    
+
+    def __str__(self):
+        return self.produto.titulo
     
 class EstiloTamanho(models.Model):
     ESTILOS_OPCOES = Camiseta.ESTILOS_OPCOES
@@ -110,28 +143,13 @@ class EstiloTamanho(models.Model):
     tamanho = models.CharField(max_length=5, choices=TAMANHOS_OPCOES)
 
     def __str__(self):
-        return f"{self.estilo} - {self.tamanho} ({self.camiseta.titulo})"
-
-class ImagemCamiseta(models.Model):
-    camiseta = models.ForeignKey(Camiseta, related_name='imagens', on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to='imagens_camisetas/', null=True)
-    principal = models.BooleanField(default=False) 
-
-    def delete(self, *args, **kwargs):
-        if self.imagem and os.path.isfile(self.imagem.path):
-            os.remove(self.imagem.path)  
-        super().delete(*args, **kwargs)
-    
-    def save(self, *args, **kwargs):
-        if self.principal:
-            ImagemCamiseta.objects.filter(camiseta=self.camiseta, principal=True).update(principal=False)
-        super().save(*args, **kwargs)
+        return f"{self.estilo} - {self.tamanho} ({self.camiseta.produto.titulo})"
         
 ####################################################################################################
 
-class Pedido(models.Model):
+class PedidoBase(models.Model):
     STATUS_OPCOES = [
-	('Pendente', 'Pendente'),
+	    ('Pendente', 'Pendente'),
         ('Pago Totalmente', 'Pago Totalmente'),
         ('Pago 1° Parcela', 'Pago 1° Parcela'),
         ('Negociando com Usuario', 'Negociando com Usuario'),        
@@ -144,24 +162,41 @@ class Pedido(models.Model):
         ('Negociar Pagamento', 'Negociar Pagamento'),
     ]
     
-    camiseta = models.ForeignKey(Camiseta, on_delete=models.CASCADE)
-    nome_estampa = models.CharField(max_length=50)
-    numero_estampa = models.CharField(max_length=50)
-    cor_escolhida = models.CharField(max_length=50, null=True)
-    tamanho = models.CharField(max_length=10)
-    estilo = models.CharField(max_length=20, default="")
+    produto = models.ForeignKey(ProdutoBase, null=True, on_delete=models.CASCADE, related_name="pedidos_base")
+    cliente = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE, related_name="pedidos")
+    
+    opcao_escolhida = models.CharField(max_length=50, null=True)
+    
     data_pedido = models.DateTimeField(auto_now_add=True)
+    
     forma_pag = models.TextField(max_length=300,null=True)
     status = models.CharField(max_length=100, default="Pendente")
     revisado = models.BooleanField(default=True)
     visto = models.BooleanField(default=False)
-    comprovante_total = models.ImageField(upload_to='qrcode_total_camisetas/', null=True, blank=True, default="")
-    comprovante_parcela1 = models.ImageField(upload_to='qrcode_total_camisetas/', null=True, blank=True, default="")
-    comprovante_parcela2 = models.ImageField(upload_to='qrcode_total_camisetas/', null=True, blank=True, default="")
-
-    cliente = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE, related_name="pedidos")
-
+    
+    comprovante_total = models.ImageField(upload_to='comprovante_total_produto/', null=True, blank=True, default="")
+    comprovante_parcela1 = models.ImageField(upload_to='comprovante_parcela1_produto/', null=True, blank=True, default="")
+    comprovante_parcela2 = models.ImageField(upload_to='comprovante_parcela2_produto/', null=True, blank=True, default="")
+    
+    
+class PedidoCamiseta(models.Model):
+    pedido = models.ForeignKey(PedidoBase, on_delete=models.CASCADE)
+    camiseta = models.ForeignKey(Camiseta, on_delete=models.CASCADE)
+    
+    nome_estampa = models.CharField(max_length=50)
+    numero_estampa = models.CharField(max_length=50)
+    
+    tamanho = models.CharField(max_length=10)
+    estilo = models.CharField(max_length=20, default="")
+    
     def __str__(self):
-        return f"Pedido para {self.camiseta.titulo} - {self.nome_estampa} ({self.numero_estampa})"
+        return f"Pedido de camiseta para {self.camiseta.produto.titulo} - {self.nome_estampa} ({self.numero_estampa})"
+
+class PedidoProduto(models.Model):
+    pedido = models.ForeignKey(PedidoBase, on_delete=models.CASCADE)
+    produto = models.ForeignKey(ProdutoDiverso, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Pedido do produto para {self.produto.produto.titulo} - {self.pedido.opcao_escolhida}"
 
 ####################################################################################################
